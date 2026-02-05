09bf0cfbc5f5d3e0a87e268ac6a02539ec5b9b23
David Ramírez Palacios <77751970+davidraamirez@users.noreply.github.com>
2026-02-05 11:01:22 +0100

    Update app.py

--- section_4/code/cd_pipeline/app.py
+++ section_4/code/cd_pipeline/app.py
@@ -5,36 +5,8 @@ import requests
 
 app = Flask(__name__)
 
-# URLs de los GIFs
-gif_urls = [
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26383-1381845104-25.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-25329-1381845415-0.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-23859-1381845509-0.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-19708-1381845008-7.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "https://www.catshaming.co.uk/wp-content/uploads/2014/11/anigif_enhanced-buzz-27162-1381845360-0.gif"
-]
-
 # Carpeta local para guardar los GIFs
 LOCAL_GIF_DIR = "static/gifs"
-
-os.makedirs(LOCAL_GIF_DIR, exist_ok=True)
-
-# Descargar los GIFs si no existen
-for url in gif_urls:
-    filename = url.split("/")[-1]
-    local_path = os.path.join(LOCAL_GIF_DIR, filename)
-    if not os.path.exists(local_path):
-        print(f"Descargando {filename}...")
-        r = requests.get(url)
-        with open(local_path, "wb") as f:
-            f.write(r.content)
 
 # Lista de GIFs locales
 gifs = os.listdir(LOCAL_GIF_DIR)
